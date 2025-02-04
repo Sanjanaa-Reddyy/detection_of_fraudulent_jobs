7f19d5e112798dd9a725643efbe540f35b575847
Sanjana Reddy Ella <98231538+Sanjanaa-Reddyy@users.noreply.github.com>
2025-02-04 11:33:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -37,13 +37,20 @@ from Preprocessing_P import OneHotEncodeLowCardinality
 from Preprocessing_P import ConvertToIntegers
 from Preprocessing_P import FeatureSelection
 from Preprocessing_P import RandomForest
+from Preprocessing_P import XGBoostClassification
+from flask import Flask, Response, url_for
 
-# Load model and preprocessing pipeline
-model_path = './model/random_forest_model.pkl'
+#storing the path for the pkl files 
+randomforest_path = './model/random_forest_model_2.pkl'
+xgboost_path= './model/xgboost_model.pkl'
 preprocessing_path = './model/preprocessing_pipeline.pkl'
 
-with open(model_path, 'rb') as model_file:
-    model = joblib.load(model_file)
+#loading the data from pkl files
+with open(randomforest_path, 'rb') as rf_model_file:
+    rf_model = joblib.load(rf_model_file)
+
+with open(xgboost_path, 'rb') as xgb_model_file:
+    xgb_model = joblib.load(xgb_model_file)
 
 with open(preprocessing_path, 'rb') as preprocessing_file:
     preprocessor = joblib.load(preprocessing_file)
@@ -53,94 +60,152 @@ app = Flask(__name__)
 def none_if_empty(arg):
     return None if arg == '' else arg
 
+#home page which displays the form 
 @app.route('/')
 def home():
-    return render_template('code.html')  # Serve the HTML form
+    return render_template('code.html') 
 
+#GET request for predict url which predicts the job submitted
 @app.route('/predict', methods=['GET'])
 def predict():
-    # Extract the query parameters from the GET request
+    #getting all the user input values from the form
     title = request.args.get('job_title')
-    print(f"job_title: {title}")  # Print statement for job_title
-
     description = none_if_empty(request.args.get('job-description'))
-    print(f"description: {description}")  # Print statement for description
-
     salary_range = none_if_empty(request.args.get('salary-range'))
-    print(f"salary_range: {salary_range}")  # Print statement for salary_range
-
     location = none_if_empty(request.args.get('job-location'))
-    print(f"job_location: {location}")  # Print statement for job_location
-
     requirements = none_if_empty(request.args.get('job-requirements'))
-    print(f"requirements: {requirements}")  # Print statement for requirements
-
     department = none_if_empty(request.args.get('department'))
-    print(f"department: {department}")  # Print statement for department
-
     company_profile = none_if_empty(request.args.get('company-profile'))
-    print(f"company_profile: {company_profile}")  # Print statement for company_profile
-
     benefits = none_if_empty(request.args.get('benefits'))
-    print(f"benefits: {benefits}")  # Print statement for benefits
-
     required_education = none_if_empty(request.args.get('required-education'))
-    print(f"required_education: {required_education}")  # Print statement for required_education
-
     required_experience = none_if_empty(request.args.get('required-experience'))
-    print(f"required_experience: {required_experience}")  # Print statement for required_experience
-
     industry = none_if_empty(request.args.get('industry'))
-    print(f"industry: {industry}")  # Print statement for industry
-
     function = none_if_empty(request.args.get('domain'))
-    print(f"domain: {function}")  # Print statement for domain
-
     employment_type = none_if_empty(request.args.get('employment-type'))
-    print(f"employment_type: {employment_type}")  # Print statement for employment_type
-
     telecommuting = none_if_empty(request.args.get('telecommuting'))
-    print(f"telecommuting_positions: {telecommuting}")  # Print statement for telecommuting_positions
-
     has_company_logo = none_if_empty(request.args.get('logo'))
-    print(f"logo: {has_company_logo}")  # Print statement for logo
-
     has_questions = none_if_empty(request.args.get('screening-questions'))
-    print(f"screening_questions: {has_questions}")  # Print statement for screening_questions
 
-
-    # List of inputs to be processed
     inputs = [
         title, description, salary_range, location, requirements, department,
         company_profile, benefits, required_education, required_experience, industry, function,
         employment_type, telecommuting, has_company_logo, has_questions
     ]
 
-    # Replace empty inputs with NaN
     inputs = [x if x else np.nan for x in inputs]
 
-    # Column names
     column_names = [
-        'title', 'description', 'salary_range', 'location', 'requirements', 'department',
+       'title', 'description', 'salary_range', 'location', 'requirements', 'department',
         'company_profile', 'benefits', 'required_education', 'required_experience', 'industry', 'function',
         'employment_type', 'telecommuting', 'has_company_logo', 'has_questions'
     ]
     
-    # Create DataFrame
     df = pd.DataFrame([inputs], columns=column_names)
-
     print(df)
     print(df.columns)
 
     processed_features = preprocessor.transform(df)
+    
+    #retriving what model did the user choose (if the user didn't choose any model, the default model choosed is random forest)
+    model_type = request.args.get('model_type', 'randomforest')  # Default is 'randomforest'
+    print(f"model: {model_type}")
 
-    # Predict the result using the XGBoost model
+    if model_type == 'randomforest':
+        model = rf_model
+    elif model_type == 'xgboost':
+        model = xgb_model
+    else:
+        return jsonify({'The model type selected is not valid'}), 400
+    
+    #passing the features i.e., the user inputs to the model for prediction
     prediction = model.predict(processed_features)
 
-    print(f"prediction: {prediction}")
+    image_url1 = url_for('static', filename='image2.jpg')#legitimate image
+    image_url2 = url_for('static', filename='image3.jpg')#fraudulent image
 
-    # Return the prediction as a JSON response
-    return jsonify({'prediction': int(prediction[0])})
+    #if the prediction is '0' i.e., the job is legitimate
+    if int(prediction[0]) == 0:
+        predict = 'The job is Legitimate'
+        html = f"""
+        <html>
+        <head><title>Prediction Result</title>
+        <style>
+        body{{
+        display: flex;
+                justify-content: center;
+                align-items: center;
+                 height: 100vh;
+                background-image: url('{image_url2}');
+        
+        background-position: center; 
+        text-align:center;
+        }}
+        .content {{
+                width: 45%;
+                border: 1px solid #ccc;
+                padding: 20px;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                background-color: rgba(255, 255, 255, 0.8); 
+                padding: 40px;
+            }}
+        </style>
+        </head>
+
+        <body>
+        <div class="content">
+        <h1>Prediction Result</h1>
+        <p><strong>Prediction:</strong> {predict}</p>
+        <p><strong>Model Name:</strong> {model_type}</p>
+        <p><strong>Job Title:</strong> {title}</p>
+        <p><strong>Department:</strong> {department}</p>
+        <p><strong>Location:</strong> {location}</p>
+        </div>
+        </body>
+        </html>
+        """
+        #if the prediction is '1' i.e., the job is fraudulent
+    else:
+        predict = 'The job is Fraduluent'
+        html= f"""
+        <html>
+        <head><title>Prediction Result</title>
+        <style>
+        body{{
+        display: flex;
+                justify-content: center;
+                align-items: center;
+                 height: 100vh;
+        background-image: 
+                    url('{image_url1}');
+                  background-position: center; 
+        text-align:center;    
+        }}
+        .content {{
+                width: 70%; 
+                border: 1px solid #ccc;
+                padding: 20px;
+                border-radius: 8px;
+                
+                background-color: white ; 
+                padding: 40px;
+            }}
+        </style>
+        </head>
+
+        <body>
+        <div class="content">
+        <h1>Prediction Result</h1>
+        <p><strong>Prediction:</strong> {predict}</p>
+        <p><strong>Model Name:</strong> {model_type}</p>
+        <p><strong>Job Title:</strong> {title}</p>
+        <p><strong>Department:</strong> {department}</p>
+        <p><strong>Location:</strong> {location}</p>
+        </div>
+        </body>
+        </html>
+        """
+    return Response(html, mimetype='text/html')
 
 if __name__ == '__main__':
     app.run(debug=True)
